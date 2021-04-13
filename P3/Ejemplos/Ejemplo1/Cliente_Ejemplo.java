e70d1031ae28a57ce358c68d0236ce8be494fb23
AlejandroFrndz <fernandezalcaidealejandro@gmail.com>
2021-04-13 17:34:42 +0200

    Ejemplo 1 practica 3 añadido

---
+++ P3/Ejemplos/Ejemplo1/Cliente_Ejemplo.java
@@ -0,0 +1,22 @@
+
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+
+public class Cliente_Ejemplo {
+    public static void main(String args[]){
+        if (System.getSecurityManager() == null){
+            System.setSecurityManager(new SecurityManager());
+        }
+
+        try {
+            String nombre_objeto_remoto = "Ejemplo_I";
+            System.out.println("Buscano el objeto remoto");
+            Registry registry = LocateRegistry.getRegistry(args[0]);
+            Ejemplo_I instancia_local = (Ejemplo_I) registry.lookup(nombre_objeto_remoto);
+            instancia_local.escribir_mensaje(Integer.parseInt(args[1]));
+        } catch (Exception e) {
+            System.err.println("Ejemplo exception:");
+            e.printStackTrace();
+        }
+    }
+}
